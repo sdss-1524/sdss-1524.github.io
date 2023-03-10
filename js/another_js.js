a56bbfabb5951853bcfec5ce8ee70a8dce04bd87
sdss-1524 <1169599200@qq.com>
2023-03-10 23:45:06 +0800

    Site updated: 2023-03-10 23:44:58

---
+++ js/another_js.js
@@ -0,0 +1,6 @@
+window.onblur=function(){
+  document.title="(PД`q。)·。'゜客官，快回来~~~";
+};
+window.onfocus=function(){
+  document.title="(╯°Д°)╯ ┻━┻  I have a crush on you"
+}
---
+++ js/weather.js
@@ -0,0 +1,25 @@
+
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "FF9900",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "borderRadius": "5",
+      "fixed": "true",
+      "vertical": "top",
+      "horizontal": "left",
+      "left": "110",
+      "top": "9",
+      "key": "2e8dc00f21b948d49f99b0dd43602ef0"
+    }
+}
